144a1c53ad6e8411cfe636586832ee4e90e9830e
Benjamín Lillo <benjalso99@gmail.com>
2020-09-27 23:09:42 -0300

    primera prubea consulta_1

--- Sites/index.php
+++ Sites/index.php
@@ -1,1 +1,24 @@
-<?php include('templates/header.html'); ?>
+<?php include('templates/header.html');   ?>
+
+<body>
+  <h1 align="center"> Entrega 2 - grupo 2 </h1>
+  <p style="text-align:center;"> Consultas </p>
+
+  <br>
+
+  <h3 align="center"> Mostrar el nombre de todas las navieras </h3>
+
+  <form align="center" action="consultas/consulta_1.php" method="post">
+
+    Nombre:
+    <input type="text" name="nombre_naviera">
+    <br/><br/>
+    <input type="submit" value="Buscar">
+  </form>
+
+  <br>
+  <br>
+  <br>
+  <br>
+</body>
+</html>
