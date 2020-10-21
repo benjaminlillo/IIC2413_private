90ef7d4e69b23eb4f9b018a7c224afb60055d921
Benjamín Lillo <benjalso99@gmail.com>
2020-10-21 19:58:17 -0300

    a

--- Sites/consultas/consultas_navegacion.php
+++ Sites/consultas/consultas_navegacion.php
@@ -24,6 +24,48 @@
     echo "<tr> <td>$p[0]</td> <td>$p[1]</td>";
   }
   ?>
-  </table>
+	</table>
+
+	<?php
+	// buques petroleros
+  $query = "SELECT s.nombre, s.patente FROM (SELECT * FROM naviera NATURAL JOIN pertenece) AS r,(SELECT * FROM buque NATURAL JOIN buquepetrolero) AS s WHERE r.bid = s.bid AND r.nid = $id;";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
+  ?>
+	<h4> Buques petroleros </h4>
+  <table>
+    <tr>
+    	<th>NOMBRE</th>
+      <th>PATENTE</th>
+    </tr>
+  <?php
+  foreach ($dataCollected as $p) {
+    echo "<tr> <td>$p[0]</td> <td>$p[1]</td>";
+  }
+  ?>
+	</table>
+
+	<?php
+	// buques de carga
+  $query = "SELECT s.nombre, s.patente FROM (SELECT * FROM naviera NATURAL JOIN pertenece) AS r,(SELECT * FROM buque NATURAL JOIN buquecarga) AS s WHERE r.bid = s.bid AND r.nid = $id;";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
+  ?>
+	<h4> Buques de carga </h4>
+  <table>
+    <tr>
+    	<th>NOMBRE</th>
+      <th>PATENTE</th>
+    </tr>
+  <?php
+  foreach ($dataCollected as $p) {
+    echo "<tr> <td>$p[0]</td> <td>$p[1]</td>";
+  }
+  ?>
+	</table>
+	
+	
 
 <?php include('../templates/footer.html'); ?>
