76c6f4f072cc6e90b5f2e443047bc337e315f0a9
Christian-Klempau <christian.klempau@uc.cl>
2020-10-22 13:26:12 -0300

    login

--- Sites/login.php
+++ Sites/login.php
@@ -12,30 +12,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
     
 </head>
 
 <body>
 
-<div class="field">
-  <label class="label">Name</label>
+<div align="center" class="field">
+  <label class="label">Pasaporte</label>
   <div class="control">
-    <input class="input" type="text" placeholder="Text input">
+    <input class="input" type="text" placeholder="n° pasaporte">
   </div>
 </div>
 
-<div class="field">
-  <label class="label">Username</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-    <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
+<div align="center" class="field">
+  <label class="label">Constraseña</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="contraseña">
   </div>
-  <p class="help is-success">This username is available</p>
 </div>
 
 </body>
