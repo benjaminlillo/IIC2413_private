fef9078a2d033b99ac8c4c4d0ff8b8b033fe5710
Maria Jose Fontanet <mjfontanet@uc.cl>
2020-11-14 20:20:40 -0300

    avance

---
+++ Entrega4/main.py
@@ -0,0 +1,79 @@
+from flask import Flask, json, request
+from pymongo import MongoClient
+
+USER = "grupo2"
+PASS = "grupo2"
+DATABASE = "grupo2"
+
+URL = f"mongodb://{USER}:{PASS}@gray.ing.puc.cl/{DATABASE}?authSource=admin"
+client = MongoClient(URL)
+
+MESSAGE_KEYS = ['date', 'lat', 'long',
+            'message', 'mid', 'receptant', 'sender']
+# Nombre bbd
+db = client["grupo2"]
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    '''
+    Página de inicio
+    '''
+    return "<h1>¡Hola!</h1>"
+
+@app.route("/users")
+def get_users():
+    '''
+        usuarios.find({}, {_id: 0})
+    '''
+    users = list(db.usuarios.find({}, {"_id": 0}))
+
+    return json.jsonify(users)
+
+
+@app.route("/users/<int:uid>")
+def get_user(uid):
+    '''
+        usuarios.find({}, {"_id": 0})
+    '''
+    users = list(db.usuarios.find({"uid": uid}, {"_id": 0}))
+
+    return json.jsonify(users)
+
+@app.route("/messages")
+def get_messages():
+    '''
+        mensajes.find({}, {_id: 0})
+    '''
+    messages = list(db.mensajes.find({}, {"_id": 0}))
+    
+    return json.jsonify(messages)
+
+@app.route("/messages/<int:mid>")
+def get_message(mid):
+    '''
+        mensajes.find({}, {"_id": 0})
+    '''
+    messages = list(db.mensajes.find({"mid": mid}, {"_id": 0}))
+    
+    return json.jsonify(messages)
+
+@app.route("/messages", methods=['POST'])
+def create_message():
+
+    data = {key: request.json[key] for key in MESSAGE_KEYS}
+
+    result = db.mensajes.insert_one(data)
+
+    return json.jsonify({"success": True})
+
+@app.route("/messages", methods=['DELETE'])
+def delete_message():
+    mid = request.json['mid']
+    db.mensajes.remove({"mid": mid})
+    return json.jsonify({"success": True})
+
+if __name__ == "__main__":
+    app.run()
+    app.run(debug=True)
