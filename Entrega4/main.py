1e8f2d287a6819244051d53f581fa4908b213b15
Christian-Klempau <christian.klempau@uc.cl>
2020-11-15 23:49:27 -0300

    text search WIP

--- Entrega4/main.py
+++ Entrega4/main.py
@@ -8,8 +8,15 @@ DATABASE = "grupo2"
 URL = f"mongodb://{USER}:{PASS}@gray.ing.puc.cl/{DATABASE}?authSource=admin"
 client = MongoClient(URL)
 
+# MENSAJES
 MESSAGE_KEYS = ['date', 'lat', 'long',
             'message', 'mid', 'receptant', 'sender']
+# TEXT-SEARCH
+SEARCH_KEYS = ['desired', 'required', 'forbidden', 'userId']
+# NO RESULTS
+no_results = {"success": True, "results": "empty", "reason": "no results"}
+# NO USER ID
+no_user_id = {"success": True, "results": "empty", "reason": "no user ID"}
 # Nombre bbd
 db = client["grupo2"]
 
@@ -73,6 +80,27 @@ def delete_message():
     mid = request.json['mid']
     db.mensajes.remove({"mid": mid})
     return json.jsonify({"success": True})
+
+
+@app.route("/text-search")
+# db.mensajes.find({$text: {$search: "salu3"}, sender: 320}, {_id: 0})
+# db.mensajes.find({$text: {$search: "salu3"}, sender: { $in: [320, 325]}}, {_id: 0})
+# db.mensajes.createIndex({"message": "text"})
+def search_message():
+    data = request.json
+    for key in SEARCH_KEYS:
+        if key not in data.keys():
+            data[key] = []
+    
+    if type(data["userId"]) == int:
+        mensajes = list(db.mensajes.find({"$text": {"$search": "salu3"}, "sender": data["userId"]}, {"_id": 0}))
+        if not mensajes:
+            return json.jsonify(no_user_id)
+        return json.jsonify(mensajes)
+    else:
+        mensajes = list(db.mensajes.find({"$text": {"$search": "salu3"}}, {"_id": 0}))
+        return json.jsonify(mensajes)
+
 
 if __name__ == "__main__":
     app.run()
